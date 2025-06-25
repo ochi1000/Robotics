c3c4f2bb11875ed1129a470ca8609fbe4d8e5aae
Ochiabuto Jideofor <chukajide@gmail.com>
2025-06-24 19:17:31 -0500

    full control

--- ps4_drive_full_control.py
+++ ps4_drive_full_control.py
@@ -1,4 +1,4 @@
-from easygopigo3 import GoPiGo3
+from gopigo3 import GoPiGo3
 from evdev import InputDevice, categorize, ecodes,list_devices
 import sys
 
@@ -11,15 +11,14 @@ def find_ps4_controller():
             return dev
     print("PS4 controller not found. Check connection")
     sys.exit(1)
-
+ 
 gamepad = find_ps4_controller()
 
-gpg = EasyGoPiGo3()
+gpg = GoPiGo3()
 
 print("Ready! Use PS4-Controller to drive. \n LEFT stick to move, X button to STOP.")
 
 NEUTRAL = 127
-MAX_INPUT = 255
 MAX_POWER =  100 # Max motor power
 DEADZONE = 15
 
@@ -35,20 +34,21 @@ def drive(left_power, right_power):
     lp = max(min(left_power, 1), -1) * MAX_POWER
     rp = max(min(right_power, 1), -1) * MAX_POWER
     
-    #Forward/backward logic
-    if lp > 0:
-        gpg.set_motor_power(gpg.MOTOR_LEFT, abs(lp))
-        gpg.motor_fwd(gpg.MOTOR_LEFT)
-    else:
-        gpg.set_motor_power(gpg.MOTOR_LEFT, abs(lp))
-        gpg.motor_bwd(gpg.MOTOR_LEFT)
+    #Apply sign for direction
+    gpg.set_motor_power(gpg.MOTOR_LEFT, lp)
+    gpg.set_motor_power(gpg.MOTOR_RIGHT, rp)
     
-    if rp > 0:
-        gpg.set_motor_power(gpg.MOTOR_RIGHT, abs(rp))
-        gpg.motor_fwd(gpg.MOTOR_RIGHT)
-    else:
-        gpg.set_motor_power(gpg.MOTOR_RIGHT, abs(rp))
-        gpg.motor_bwd(gpg.MOTOR_RIGHT)
+    #Forward/backward logic
+    #if lp > 0:
+        #gpg.set_motor_direction(gpg.MOTOR_LEFT, gpg.MOTOR_FORWARD)
+    #else:
+        #gpg.set_motor_direction(gpg.MOTOR_LEFT, gpg.MOTOR_BACKWARD)
+    
+    #if rp > 0:
+        #gpg.set_motor_direction(gpg.MOTOR_RIGHT, gpg.MOTOR_FORWARD)
+
+    #else:
+        #gpg.set_motor_direction(gpg.MOTOR_RIGHT, gpg.MOTOR_BACKWARD)
         
 
 #Main loop
@@ -60,14 +60,15 @@ for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
         if event.code == 304 and event.value == 1: #X button
             print("STOP")
-            gpg.stop()
+            gpg.set_motor_power(gpg.MOTOR_LEFT, 0)
+            gpg.set_motor_power(gpg.MOTOR_RIGHT, 0)
     
     elif event.type == ecodes.EV_ABS: 
         if event.code == 1: #Left stick Y-axis
             if within_deadzone(event.value):
                 current_y = 0.0 #pass # Do nothing if near center
             else:
-                current_y = -scale(event.value) #Y is inverted
+                current_y = scale(event.value) #Y is inverted
 
         elif event.code == 0: #Left Stick X-axis
             if within_deadzone(event.value):
